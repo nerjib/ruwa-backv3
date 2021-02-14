273853521db5883a86561297145178ae8b1df54a
nerjib <kabirnajib0@gmail.com>
2021-02-14 18:34:22 +0100

    get supervisors id

--- src/controllers/supervisorinfo.js
+++ src/controllers/supervisorinfo.js
@@ -19,7 +19,7 @@ const db = require('../db/index');
   });  
   
   router.get('/local/:phase', async (req, res) => {
-    const getAllQ = `select users.id,users.first_name, users.last_name, users.other_name, projects.title, projects.lot, avg(projects.totalcov), users.actno, users.bank from projects left join users on projects.local_id=users.id  where projects.phase=$1  group by users.id projects.title, projects.lot, users.actno, users.bank, users.first_name, users.last_name, users.other_name order by projects.title asc,users.id asc, projects.lot asc`;
+    const getAllQ = `select users.id,users.first_name, users.last_name, users.other_name, projects.title, projects.lot, avg(projects.totalcov), users.actno, users.bank from projects left join users on projects.local_id=users.id  where projects.phase=$1  group by users.id, projects.title, projects.lot, users.actno, users.bank, users.first_name, users.last_name, users.other_name order by projects.title asc,users.id asc, projects.lot asc`;
     try {
       // const { rows } = qr.query(getAllQ);
       const { rows } = await db.query(getAllQ,[req.params.phase]);
