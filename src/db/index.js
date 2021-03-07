bd1884e647e4267d614081b6eb48eee3736d0a34
nerjib <kabirnajib0@gmail.com>
2021-03-08 00:01:18 +0100

    ssl

--- src/db/index.js
+++ src/db/index.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const client = new Client({
   connectionString: process.env.DATATYPE === 'test' ? process.env.DATABASE_URL1 : process.env.DATABASE_URL,
-
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 client.connect();
