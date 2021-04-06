92c46a4c159f32e556ad353fec80077c258fa029
nerjib <kabirnajib0@gmail.com>
2021-04-06 11:02:31 +0100

    twillio package

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const Reports = require('./src/controllers/reports');
 const Update = require('./src/controllers/updates')
 const VLC = require('./src/controllers/vlc')
 const SupaInfo = require('./src/controllers/supervisorinfo')
-//const USSD = require('./src/controllers/ussd')
+const USSD = require('./src/controllers/ussd')
 
 
 
