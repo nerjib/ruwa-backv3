5594d6198f658286d9e723c22fafa0918c2679f0
nerjib <kabirnajib0@gmail.com>
2021-07-19 21:26:00 +0100

    get projects and functionality

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -329,7 +329,7 @@ return res.status(400).send(error);
 
   router.get('/projectsfunctionality', async (req, res) => {
    // const getAllQ = `SELECT  projects.functionality,projects.lastdate,projects.started,projects.totalcov,projects.valuation,projects.gps,projects.community,projects.facility,projects.pstatus,projects.id,projects.title,projects.lga,projects.ward,projects.status, followupreports.functionality,followupreports.cause, followupreports.problem,followupreports.problemduration,followupreports.remark, followupreports.cordinate,followupreports.time,followupreports.gentime from Projects left join followupreports on projects.id = followupreports.pid order by projects.title desc, projects.id desc`;
-   const getAllQ = `SELECT  projects.functionality,projects.lastdate,projects.started,followupreports.cause,followupreports.problemduration, followupreports.cordinate from Projects left join followupreports on projects.id = followupreports.pid`;
+   const getAllQ = `SELECT  projects.functionality,projects.lastdate,projects.started,followupreports.cause,followupreports.problemduration, followupreports.cordinate from followupreports left join projects on projects.id = followupreports.pid`;
  
    try {
       // const { rows } = qr.query(getAllQ);
