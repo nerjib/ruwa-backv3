188e3818496bc071196582bfcba5b2f222d77b91
nerjib <kabirnajib0@gmail.com>
2021-10-30 12:06:48 +0100

    odf feature

--- src/controllers/odf.js
+++ src/controllers/odf.js
@@ -42,6 +42,19 @@ router.post('/', async (req, res) => {
       return res.status(400).send(`${error} jsh`);
     }
   });  
+  router.get('/details/:id', async (req, res) => {
+    const getAllQ = 'SELECT * FROM odf where id=$1';
+    try {
+      // const { rows } = qr.query(getAllQ);
+      const { rows } = await db.query(getAllQ,[req.params.id]);
+      return res.status(201).send(rows);
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).send({ message: 'User with that EMAIL already exist' });
+      }
+      return res.status(400).send(`${error} jsh`);
+    }
+  });  
 
   
 
