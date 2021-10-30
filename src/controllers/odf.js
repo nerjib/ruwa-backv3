3e8ee9ad96af24f84dc02785f1285f86fdb356ff
nerjib <kabirnajib0@gmail.com>
2021-10-30 13:07:56 +0100

    odf update

--- src/controllers/odf.js
+++ src/controllers/odf.js
@@ -56,6 +56,20 @@ router.post('/', async (req, res) => {
     }
   });  
 
+  router.post('/updateod', async (req, res) => {
+    const getAllQ = `UPDATE odf set no_of_ward=$1, no_of_communities=$2, no_of_triggered=$3, no_of_untriggered=$4, no_of_certified=$5, gap=$6 where id=$7`;
+    try {
+      // const { rows } = qr.query(getAllQ);
+      const { rows } = await db.query(getAllQ,[req.body.ward,req.body.communities, req.body.triggered, req.body.untriggered,req.body.certified, req.body.gap, req.body.id]);
+      return res.status(201).send(rows);
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).send({ message: 'User with that EMAIL already exist' });
+      }
+      return res.status(400).send(`${error} jsh`);
+    }
+  });
+  
   
 
 
